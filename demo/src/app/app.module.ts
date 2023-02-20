0dcba4f240af27702a5918d517eaa0bb11072463
tati oliveira <tat1roliv@gmail.com>
2023-02-20 19:48:26 -0300

    --delete files

--- demo/src/app/app.module.ts
+++ demo/src/app/app.module.ts
@@ -8,14 +8,11 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { FormStudentComponent } from './components/form-student/form-student.component';
-import { MainContentComponent } from './components/main-content/main-content.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
-import { StudentsListComponent } from './components/students-list/students-list.component';
 import { ToolbarComponent } from './components/toolbar/toolbar.component';
 import { MaterialModule } from './material.module';
 import { StudentNamePipe } from './pipes/student-name.pipe';
 import { TitleSizeDirective } from './directives/title-size.directive';
-import { StudentsTempComponent } from './components/students-temp/students-temp.component';
 
 
 
@@ -24,13 +21,10 @@ import { StudentsTempComponent } from './components/students-temp/students-temp.
         AppComponent,
         ToolbarComponent,
         NavbarComponent,
-        MainContentComponent,
         FooterComponent,
-        StudentsListComponent,
         FormStudentComponent,
         StudentNamePipe,
         TitleSizeDirective,
-        StudentsTempComponent,
 
     ],
     providers: [],
