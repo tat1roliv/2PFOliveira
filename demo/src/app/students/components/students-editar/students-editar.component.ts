5af4bf4d711ef5bb4ff2d24068de960551bfeaa8
tati oliveira <tat1roliv@gmail.com>
2023-02-23 16:56:38 -0300

    addstudent ts

--- demo/src/app/students/components/students-editar/students-editar.component.ts
+++ demo/src/app/students/components/students-editar/students-editar.component.ts
@@ -2,8 +2,6 @@ import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup , Validators} from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
 import { StudentsService } from '../../services/students.service';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatInputModule } from '@angular/material/input';
 import { Student } from 'src/app/models/student';
 
 
